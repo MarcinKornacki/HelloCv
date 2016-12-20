a4f3ab49cb11a4c68abee55f464c2bcfe6bad727
Marcin Kornacki <marcin.kornacki@polidea.com>
2016-12-20 09:37:51 +0100

    Added opencv full setup

--- openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Owner\\AndroidStudioProjects\\HelloCv\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /Users/marcinkornacki/StudioProjects/HelloCv/openCVLibrary310/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
